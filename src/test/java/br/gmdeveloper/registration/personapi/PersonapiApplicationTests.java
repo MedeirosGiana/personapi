c980bf73ee5da24500b5525e9dc6632bb4df3e61
giana medeiros <gianamedeiros.00510@gmail.com>
2023-05-23 17:10:45 -0300

    project create

---
+++ src/test/java/br/gmdeveloper/registration/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gmdeveloper.registration.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
